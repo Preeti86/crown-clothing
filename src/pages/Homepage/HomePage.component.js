d012a575e32f06de96237ed5a7a819d3526a1c65
Preeti Ambekar <preeti.ambekar@thomscook.se>
2020-02-29 23:31:15 +0100

    structured the homepage into smaller components, finished styling the menu item

---
+++ src/pages/Homepage/HomePage.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.style.scss';
+import directory from '../../components/directory/directory.component';
+import Directory from '../../components/directory/directory.component';
+
+
+const HomePage = () => (
+  <div className='HomePage'>
+    <Directory />
+  </div>
+)
+
+export default HomePage;
