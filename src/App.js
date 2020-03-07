287b4f4b3d30976f9341b8409b8ba67bae9167e4
Preeti Ambekar <preeti.ambekar@thomscook.se>
2020-03-07 20:30:42 +0100

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-
 import HomePage from './pages/Homepage/HomePage.component';
 import shopPage from './pages/shop/shop.component'; 
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -21,10 +20,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(Snapshot => {
+          this.setState ({
+            currentUser: {
+              id: Snapshot.id,
+              ...Snapshot.data()
+            }
+           });
+        });
+      }
+      this.setState( {currentUser: userAuth} )
     });
   }
 
