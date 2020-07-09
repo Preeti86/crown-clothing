d0fc1c1d6282f08c07439fa22b9c229789defd73
Preeti Ambekar <preeti@Preetis-MBP.lan>
2020-07-09 18:52:40 +0200

    update stripe

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckButton = ({ price }) => {
-    const priveForStripe = price * 100;
+    const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51H2dqKLngF7daYxqFaQzLotJQfqdZNs3ln66vN1um4RvSRj54VgYEnicU7v3IBcZCLbBLHczDBgbtxxtKtgM0Iyq00tndnIMNh';
 
     const onToken = token => {
